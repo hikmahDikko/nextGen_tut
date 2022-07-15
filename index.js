c92fb5f36c05e65f4a3a14f94effff3225928c24
hikmahDikko <dhywis31@gmail.com>
2022-07-15 13:10:45 +0100

    Add index.js

--- index.js
+++ index.js
@@ -8,3 +8,18 @@ console.log(hikmahDetails[1]);
 console.log(hikmahDetails[2]);
 console.log(hikmahDetails[3]);
 console.log(hikmahDetails[4]);
+
+let details={
+    name: "Hikmat Dikko",
+    age: 16,
+    workDetails: ["Frontend Developer", 
+        "Backend Developer", "Node Developer"],
+    fullName: function (){
+        return this.name;
+    }
+}
+
+console.log(details.name);
+console.log(details.age);
+console.log(details.workDetails);
+console.log(details.fullName());
