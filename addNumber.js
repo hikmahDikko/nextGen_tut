6eb9a81d109d2c7b03cdf30e62135b87399e7d48
hikmahDikko <dhywis31@gmail.com>
2022-07-15 10:18:04 +0100

    Add addNumber.js and main.js

---
+++ addNumber.js
@@ -0,0 +1,5 @@
+function addNumber(a, b) {
+    console.log(a + b);
+}
+
+addNumber(5, 7);
---
+++ main.js
@@ -0,0 +1,12 @@
+let firstNum = 5;
+let lastNum = 7;
+
+function addNumber(){
+    let result = firstNum + lastNum;
+    console.log(result);
+}
+
+addNumber();
+
+let myName = "Dikko Hikmat"
+console.log(myName);
