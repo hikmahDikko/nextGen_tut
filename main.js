d20879440a947565b734326aa6e221368e858429
hikmahDikko <dhywis31@gmail.com>
2022-07-16 13:40:58 +0100

    Add main.js

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ let lastNum = 7;
 console.log(firstNum);
 
 function addNumber(){
-    let firstNum = 18;
-    console.log(firstNum);
-    let result = firstNum + lastNum;
-    console.log(result);
+let firstNum = 18;
+console.log(firstNum);
+let result = firstNum + lastNum;
+console.log(result);
 }
 
 addNumber();
@@ -22,11 +22,11 @@ let isNotSick = true;
 //let isNotSick = false;
 
 function hikmatStatus(){
-    if(isNotSick){
-        console.log("Hikmat is not sick.");
-    }else{
-        console.log("Hikmat is sick.");
-    }
+if(isNotSick){
+    console.log("Hikmat is not sick.");
+}else{
+    console.log("Hikmat is sick.");
+}
 }
 
 hikmatStatus();
@@ -36,21 +36,39 @@ hikmatStatus();
 let age = 21;
 let message = '';
 if (age < 3) {
-    message = 'Hi, baby!';
-  } else if (age < 18) {
-    message = 'Hello!';
-  } else if (age < 100) {
-    message = 'Greetings!';
-  } else {
-    message = 'What an unusual age!';
-  }
-  console.log(message);
+message = 'Hi, baby!';
+} else if (age < 18) {
+message = 'Hello!';
+} else if (age < 100) {
+message = 'Greetings!';
+} else {
+message = 'What an unusual age!';
+}
+console.log(message);
 
-  //Alternatively using ? comparison
-  let age2 = 10000;
-  let message2 = console.log(
-      age2 < 3 ? "Hi, baby" :
-      age2 < 18 ? "Hello" :
-      age2 < 100 ? "Greetimgs" :
-      "What an unusual age!"
-  );
+//Alternatively using ? comparison
+let age2 = 10000;
+let message2 = console.log(
+    age2 < 3 ? "Hi, baby" :
+    age2 < 18 ? "Hello" :
+    age2 < 100 ? "Greetimgs" :
+    "What an unusual age!"
+);
+
+//Null coalescing operator ??
+let name = "Hkmah";
+console.log(name ?? null);
+
+//Prime numbers check between 1 to 10
+let n = 10;
+
+nextPrime:
+for (let i = 2; i <= n; i++) {
+
+    for (let j = 2; j < i; j++) { 
+        if (i % j == 0) 
+            continue nextPrime;
+    }
+
+    console.log( i ); // a prime
+}
